e97807c584c4387661b84b126c41a9cc024a013a
Juan Pablo Royo <juanpablo.royo@gmail.com>
2017-09-19 02:33:40 +0200

    NA

---
+++ streams/src/main/scala/test.sc
@@ -0,0 +1,7 @@
+object test {
+
+  import streams.Bloxorz._
+
+  Block(Pos(1,1),Pos(1,1))
+
+}
--- streams/src/test/scala/streams/BloxorzSuite.scala
+++ streams/src/test/scala/streams/BloxorzSuite.scala
@@ -78,4 +78,32 @@ class BloxorzSuite extends FunSuite {
     }
   }
 
+
+  test("test legalNeighboursWith History for example") {
+    new Level1 {
+      val expected = Set(
+        (Block(Pos(1,2),Pos(1,3)), List(Right,Left,Up)),
+        (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+      )
+      assert(neighborsWithHistory(Block(Pos(1,1),Pos(1,1)), List(Left,Up)).toSet === expected)
+    }
+  }
+
+  test("test newNeighborsOnly for example") {
+    new Level1 {
+      val expected = Set(
+        (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+      ).toStream
+      val toAssert = newNeighborsOnly(
+        Set(
+          (Block(Pos(1,2),Pos(1,3)), List(Right,Left,Up)),
+          (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+        ).toStream,
+
+        Set(Block(Pos(1,2),Pos(1,3)), Block(Pos(1,1),Pos(1,1)))
+      )
+      assert(toAssert.toSet === expected.toSet)
+    }
+  }
+
 }
